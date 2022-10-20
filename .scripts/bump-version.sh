fb61db90472291b3ebf08508e9b988312d71b851
Viet Pham <viet@webonyx.com>
2022-10-20 18:35:10 +0700

    No auto merge

--- .scripts/bump-version.sh
+++ .scripts/bump-version.sh
@@ -7,5 +7,5 @@ echo RUNNING FROM "$0"
 VERSION=$(cat ./VERSION)
 
 # shellcheck disable=SC2046
-# git commit -am "Bump package.json version to v${VERSION} [skip ci]"
-# composer update
+composer config version $(cat VERSION)
+ git commit -am "Bump composer version to v${VERSION} [skip ci]"
