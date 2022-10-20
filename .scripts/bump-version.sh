d1409165d81f5fece6e2bb48c6d6de7985301764
Viet Pham <viet@webonyx.com>
2022-10-20 18:17:00 +0700

    file-version

---
+++ .scripts/bump-version.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -xe
+
+echo RUNNING FROM "$0"
+
+VERSION=$(cat ./VERSION)
+
+# shellcheck disable=SC2046
+# git commit -am "Bump package.json version to v${VERSION} [skip ci]"
