914ea7328bc6f73fdded712b6395d6e72cc6a3e2
Viet Pham <viet@webonyx.com>
2022-10-20 18:39:57 +0700

    develop

--- .scripts/bump-version.sh
+++ .scripts/bump-version.sh
@@ -8,4 +8,8 @@ VERSION=$(cat ./VERSION)
 
 # shellcheck disable=SC2046
 composer config version $(cat VERSION)
- git commit -am "Bump composer version to v${VERSION} [skip ci]"
+git commit -am "Bump composer version to v${VERSION} [skip ci]"
+
+git checkout develop
+git merge production -m "Merge from production"
+git push origin develop
