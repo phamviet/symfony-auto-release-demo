68aab4fe79d5e4b4f82355d4bde0e1fb5d2bc286
Viet Pham <phamviet@users.noreply.github.com>
2022-11-17 17:49:23 +0700

    feat: add release example commands

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Usage ./scripts/release.sh
+# Usage
+# ./scripts/release.sh
+# ./scripts/release.sh --label minor
 
 title=$(date +"release-%H%M-%F")
 gh pr create --base master --head develop --label release --title "$title" --fill "$@"
