9e19552dfe8de759942b70d041e08b07c4e31f83
Viet Pham <phamviet@users.noreply.github.com>
2022-11-17 09:04:35 +0700

    patch

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -3,4 +3,4 @@
 # Usage ./scripts/release.sh
 
 title=$(date +"release-%H%M-%F")
-gh pr create --base master --head develop --label release --title "$title" --fill "$@"
+gh pr create --base master --head develop --label release --label patch --title "$title" --fill "$@"
